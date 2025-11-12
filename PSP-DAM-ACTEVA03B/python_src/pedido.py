0beef58db36ce77399592469e0a7988d02ac5ed8
IgnacioAyllon18 <i.ayllon@pro2fp.es>
2025-11-12 15:36:49 +0100

    Delete PSP-DAM-ACTEVA03B directory

--- PSP-DAM-ACTEVA03B/python_src/pedido.py
+++
@@ -1,7 +0,0 @@
-class Pedido:
-    def __init__(self, id, nombre):
-        self.id = id
-        self.nombre = nombre
-
-    def __repr__(self):
-        return f"Pedido(id={self.id}, nombre='{self.nombre}')"
